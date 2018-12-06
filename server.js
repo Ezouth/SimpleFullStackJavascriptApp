ebba4658c014b8d9152c96a0979cbf22a9c02961
Tyler <Ty.whidden@gmail.com>
2018-12-05 21:36:23 -0500

    Test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 5000;
+app.use(express.static(path.join('ReadNode_Solstice', 'client/build')));
 
 // first thing to do is parse the json file
 const utilityDataObject = require('./data.json');
